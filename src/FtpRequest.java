2c654849b4a16db1d6f38c958364f7b27a270b3f
radgeur <remy2112@hotmail.fr>
2016-02-04 11:54:24 +0100

    methods in ftpRequest

--- src/FtpRequest.java
+++ src/FtpRequest.java
@@ -1,45 +1,84 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 
 
 public class FtpRequest implements Runnable{
 	
 	private Socket s;
-	private OutputStream output;
+	private PrintStream print;
 	private BufferedReader br;
+	private String currentRepository;
 	
-	public FtpRequest(Socket s) throws IOException{
-		System.out.println("création de la socket");
+	public FtpRequest(Socket s, String repo) throws IOException{
 		this.s=s;
-		System.out.println("Création de la sortie");
-		this.output = this.s.getOutputStream();
-		System.out.println("création de la lecture");
+		print = new PrintStream(this.s.getOutputStream());
 		this.br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		System.out.println("lecture");
-		//br.readLine(); Récupération d'un message
-		System.out.println("lu");
-	}
-	
-	public void ProcessRequest(){
-		
+		currentRepository = repo;
 	}
 
+	public void write(String string) {
+		print.println(string);
+	}
+	
+	public String receive() throws IOException {
+		return br.readLine();
+	}
+	
+	public void ProcessRequest(String string){
+		switch(string) {
+			case "user":
+				;
+			case "pass":
+				;
+			case "retr":
+				;
+			case "store":
+				;
+			case "quit":
+				;
+			default:
+				return;
+		}
+	}
+	
+	public void processUSER() {
+		
+	}
+	
+	public void processPASS() {
+		
+	}
+	
+	public void processRETR() {
+		
+	}
+	
+	public void processSTOR() {
+		
+	}
+	
+	public void processLIST() {
+		
+	}
+	
+	public void processQUIT() throws IOException {
+		s.close();
+	}
+	
 	@Override
 	public void run() {
-		System.out.println("it works");
-		try {
-			output.write("200 Service OK".getBytes());
-		} catch (IOException e) {
-			e.printStackTrace();
+		this.write("200 Service OK");
+		while(true) {
+			String string = null;
+			try {
+				string = receive();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			ProcessRequest(string);
 		}
-		try {
-			output.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
 	}
 }
--- src/Serveur.java
+++ src/Serveur.java
@@ -1,50 +1,30 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Serveur {
 
 	private ServerSocket port;
-	private OutputStream output;
-	private BufferedReader br;
 	
-	public Serveur(){
+	public Serveur() throws IOException{
 		try {
 			port = new ServerSocket(1028);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//this.output = this.port.;
-		//this.br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		this.attendreCx();
 	}
 	
 	public void attendreCx() throws IOException{
 		while(true){
 			Socket s=port.accept();
-			Thread t = new Thread(new FtpRequest(s));
-			System.out.println("coucou");
+			Thread t = new Thread(new FtpRequest(s, "./"));
 			t.start();
 		}
 	}
 	
-	public void recevoir() throws IOException{
-		this.br.readLine();
-		
-	}
-	
 	public static void main(String[] args) throws IOException {
-		Serveur s = new Serveur();
-		System.out.println("création du client");
-		Socket client = new Socket(InetAddress.getLocalHost(), 1028);
-		BufferedReader br_client = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		System.out.println("Attente du message");
-		s.attendreCx();
-		System.out.println("tentative de reception de message");
-		br_client.readLine();
-		System.out.println("message reçu");
+		new Serveur();
+		
 	}
 }
